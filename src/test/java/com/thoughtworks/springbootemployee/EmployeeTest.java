e1cbd8e45c06f93e15e66c77b119c5c2f4b6cc98
CHENRO6 <chenro6@oocl.com>
2020-07-29 20:25:15 +0800

    [spring-boot-employee][olivia-roy] test: should_return_exception_when_get_employee_given_a_not_existed_employee_id

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeTest.java
@@ -1,20 +1,19 @@
 package com.thoughtworks.springbootemployee;
 
 import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.exception.EmployeeNotFoundException;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
-import com.thoughtworks.springbootemployee.service.EmployeeService;
 import com.thoughtworks.springbootemployee.service.impl.EmployeeServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.exceptions.base.MockitoException;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,14 +27,24 @@ public class EmployeeTest {
 
     @Test
     void should_return_a_employee_when_get_employee_given_a_employee() {
-        //given
+        // given
         Integer id = 1;
         Employee employee = new Employee();
         employee.setId(id);
-        //when
+        // when
         when(employeeRepository.findById(id)).thenReturn(Optional.of(employee));
         Employee actualEmployee = employeeService.getEmployeeById(id);
-        //then
+        // then
         assertEquals(employee, actualEmployee);
+    }
+
+    @Test
+    void should_return_exception_when_get_employee_given_a_not_existed_employee_id() {
+        // given
+        Integer id = 2;
+        // when
+        when(employeeRepository.findById(id)).thenReturn(Optional.empty());
+        // then
+        assertThrows(EmployeeNotFoundException.class, () -> employeeService.getEmployeeById(id));
     }
 }
