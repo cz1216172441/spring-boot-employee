ee0187eb7a9ac104cae7b826c81f4184ea207454
CHENRO6 <chenro6@oocl.com>
2020-07-29 21:03:30 +0800

    [spring-boot-employee][olivia-roy] feature: create get all companies API

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.springbootemployee.service.impl;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    @Autowired
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> getAllCompany() {
+        return companyRepository.findAll();
+    }
+}
