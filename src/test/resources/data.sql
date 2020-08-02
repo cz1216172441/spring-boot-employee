f7904d501d826f0de44b1766e2ffe8444a9513ee
CHENRO6 <chenro6@oocl.com>
2020-08-02 17:33:39 +0800

    [spring-boot-employee][roy-max] feature: pass should_return_1_employee_when_add_employee_given_1_employee_request_dto

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -7,13 +7,7 @@ CREATE TABLE employee (
   gender VARCHAR(10) DEFAULT NULL,
   company_id INT Not NULL
 );
-INSERT INTO employee (name, age, gender,company_id) VALUES
- ('chengcheng','54','male',1),
- ('yuexie','82','female',1),
- ('haifeng','69','female',1),
- ('ang','18','male',1);
  CREATE TABLE company (
    company_id INT AUTO_INCREMENT  PRIMARY KEY,
    name VARCHAR(100) NOT NULL
  );
- INSERT INTO company (name) VALUES('tw');
